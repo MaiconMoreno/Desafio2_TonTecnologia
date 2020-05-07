3e1d4a8d6f1d532174f5372e6b501f63bb6c62f8
Maicon Moreno <maiconmoreno@hotmail.com>
2020-05-07 18:29:44 -0300

    Remoção da pasta utils

---
+++ src/utils/logger.js
@@ -0,0 +1,29 @@
+
+
+const { createLogger, format, transports } = require('winston');
+
+
+module.exports = createLogger({
+
+    format: format.combine(
+
+        format.timestamp({ format: 'DD/MM/YYYY HH:MM:ss' }),
+        format.errors({ stack: true }),
+        format.json(),
+        format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
+    ),
+
+    transports: [
+
+        new transports.File({
+            maxsize: 5000000,
+            maxFiles: 5,
+            filename: `${__dirname}/../logs/log-api.log`
+        }),
+
+        new transports.Console({
+            level: 'debug'
+        })
+    ]
+});
+
