29b8e7a422f4142ad8d42d0795621d0f5c9e3716
Maicon Moreno <maiconmoreno@hotmail.com>
2020-05-07 18:28:46 -0300

    Atualização da index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
-const tabelas = require('./infraestrutura/tabelas');
+const looger = require('..//src/utils/logger');
+
+const app = customExpress();
+
+//const tabelas = require('./infraestrutura/tabelas');
+
 
 conexao.connect((erro => {
+
     if (erro) {
         console.log(erro);
     } else {
         console.log('Conectado com sucesso!')
+        //tabelas.init(conexao);   
+        const porta = 3000;
 
-        tabelas.init(conexao);
-        const app = customExpress();
-
-        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+        app.listen(porta, () => looger.info(`Servidor online, rodando na porta ${porta}`));
     }
 })
 );
